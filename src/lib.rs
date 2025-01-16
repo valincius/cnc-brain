1d2c2e1d7135d02819699d2eaa5a715f6bc8aed6
John <john@valincius.dev>
2025-01-16 01:43:24 +0000

    Refactor stepper implementation and restore lib.rs; update VSCode settings

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![no_std]
+
+pub mod runner;
+pub mod stepper;
+
+#[derive(Debug, Default, Copy, Clone)]
+pub struct Coordinates {
+    pub x: Option<f32>,
+    pub y: Option<f32>,
+    pub z: Option<f32>,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Motion {
+    Rapid(Coordinates),
+    Linear { feedrate: f32, coords: Coordinates },
+}
--- src/stepper.rs
+++ src/stepper.rs
@@ -1,54 +1,54 @@
-// pub struct Stepper {
-//     pub pin: u8,
-// }
+pub struct Stepper {
+    pub pin: u8,
+}
 
-// impl Stepper {
-//     pub fn new(pin: u8) -> Self {
-//         Stepper { pin }
-//     }
-// }
+impl Stepper {
+    pub fn new(pin: u8) -> Self {
+        Stepper { pin }
+    }
+}
 
-// impl Motor for Stepper {
-//     fn on(&self, speed: Option<f32>, direction: Option<Direction>) {
-//         println!("Stepper on");
-//     }
+impl Motor for Stepper {
+    fn on(&self, speed: Option<f32>, direction: Option<Direction>) {
+        // println!("Stepper on");
+    }
 
-//     fn on_time(&self, time: f32, speed: Option<f32>, direction: Option<Direction>) {
-//         println!("Stepper on for {} seconds", time);
-//     }
+    fn on_time(&self, time: f32, speed: Option<f32>, direction: Option<Direction>) {
+        // println!("Stepper on for {} seconds", time);
+    }
 
-//     fn on_cw(&self, speed: Option<f32>) {
-//         println!("Stepper on clockwise");
-//     }
+    fn on_cw(&self, speed: Option<f32>) {
+        // println!("Stepper on clockwise");
+    }
 
-//     fn on_ccw(&self, speed: Option<f32>) {
-//         println!("Stepper on counter-clockwise");
-//     }
+    fn on_ccw(&self, speed: Option<f32>) {
+        // println!("Stepper on counter-clockwise");
+    }
 
-//     fn on_time_cw(&self, time: f32, speed: Option<f32>) {
-//         println!("Stepper on clockwise for {} seconds", time);
-//     }
+    fn on_time_cw(&self, time: f32, speed: Option<f32>) {
+        // println!("Stepper on clockwise for {} seconds", time);
+    }
 
-//     fn on_time_ccw(&self, time: f32, speed: Option<f32>) {
-//         println!("Stepper on counter-clockwise for {} seconds", time);
-//     }
+    fn on_time_ccw(&self, time: f32, speed: Option<f32>) {
+        // println!("Stepper on counter-clockwise for {} seconds", time);
+    }
 
-//     fn off(&self) {
-//         println!("Stepper off");
-//     }
-// }
+    fn off(&self) {
+        // println!("Stepper off");
+    }
+}
 
-// pub trait Motor {
-//     fn on(&self, speed: Option<f32>, direction: Option<Direction>);
-//     fn on_time(&self, time: f32, speed: Option<f32>, direction: Option<Direction>);
-//     fn on_cw(&self, speed: Option<f32>);
-//     fn on_ccw(&self, speed: Option<f32>);
-//     fn on_time_cw(&self, time: f32, speed: Option<f32>);
-//     fn on_time_ccw(&self, time: f32, speed: Option<f32>);
-//     fn off(&self);
-// }
+pub trait Motor {
+    fn on(&self, speed: Option<f32>, direction: Option<Direction>);
+    fn on_time(&self, time: f32, speed: Option<f32>, direction: Option<Direction>);
+    fn on_cw(&self, speed: Option<f32>);
+    fn on_ccw(&self, speed: Option<f32>);
+    fn on_time_cw(&self, time: f32, speed: Option<f32>);
+    fn on_time_ccw(&self, time: f32, speed: Option<f32>);
+    fn off(&self);
+}
 
-// pub enum Direction {
-//     Forward,
-//     Backward,
-// }
+pub enum Direction {
+    Forward,
+    Backward,
+}
