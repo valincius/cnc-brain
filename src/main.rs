3dc56d3db2c38d7aee1d1742b5c749cf5ff072d8
John Valincius <john@valincius.dev>
2025-02-14 20:37:04 +0000

    use channel for pushing steps

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use core::str;
 
 use assign_resources::assign_resources;
 use embassy_executor::{Executor, Spawner};
+use embassy_rp::dma::Channel as _;
 use embassy_rp::multicore::{spawn_core1, Stack};
+use embassy_rp::pac::PIO0;
 use embassy_rp::pio::Config;
 use embassy_rp::{
     bind_interrupts,
     gpio::{Level, Output},
-    peripherals::{PIO0, USB},
+    peripherals::{PIO0 as pPIO0, USB as pUSB},
     pio::{Pio, StateMachine},
     rom_data::reset_to_usb_boot,
 };
@@ -37,9 +39,14 @@ static CONTROLLER_CHANNEL: embassy_sync::channel::Channel<
 static MOTION_QUEUE: embassy_sync::channel::Channel<CriticalSectionRawMutex, MotionCommand, 128> =
     Channel::new();
 
+static STEP_QUEUE: embassy_sync::channel::Channel<CriticalSectionRawMutex, StepBuffer, 2> =
+    Channel::new();
+
+struct StepBuffer([u32; 1024]);
+
 bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
-    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<USB>;
+    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<pPIO0>;
+    USBCTRL_IRQ => embassy_rp::usb::InterruptHandler<pUSB>;
 });
 
 assign_resources! {
@@ -52,7 +59,8 @@ assign_resources! {
         pio: PIO0,
         x_step: PIN_0,
         x_dir: PIN_1,
-        x_step_buf: DMA_CH0,
+
+        step_dma0: DMA_CH0,
     }
 }
 
@@ -93,9 +101,7 @@ async fn core0_main(r: Core0Resources, spawner: Spawner) {
 async fn core1_main(r: Core1Resources, spawner: Spawner) {
     let Pio {
         mut common,
-        sm0: mut x_sm,
-        sm1,
-        sm2,
+        mut sm0,
         ..
     } = Pio::new(r.pio, Irqs);
 
@@ -128,15 +134,16 @@ async fn core1_main(r: Core1Resources, spawner: Spawner) {
     let x_step = common.make_pio_pin(r.x_step);
     let x_dir = Output::new(r.x_dir, Level::Low);
 
-    let mut x_cfg = Config::default();
-    x_cfg.set_set_pins(&[&x_step]);
-    x_cfg.use_program(&stepper_prog, &[]);
-    x_cfg.clock_divider = (104.17).to_fixed(); // 125MHz / 104.17 = 1.2MHz = we are aiming for 200khz max pulse rate (5us per pulse), each pulse is 6 cycles
+    let mut step_pio_cfg = Config::default();
+    step_pio_cfg.set_set_pins(&[&x_step]);
+    step_pio_cfg.use_program(&stepper_prog, &[]);
+    step_pio_cfg.clock_divider = (104.17).to_fixed(); // 125MHz / 104.17 = 1.2MHz = we are aiming for 200khz max pulse rate (5us per pulse), each pulse is 6 cycles
 
-    x_sm.set_config(&x_cfg);
-    x_sm.set_enable(true);
+    sm0.set_config(&step_pio_cfg);
+    sm0.set_enable(true);
 
-    spawner.spawn(controller(x_sm, r.x_step_buf)).unwrap();
+    spawner.spawn(controller()).unwrap();
+    spawner.spawn(step_consumer(sm0, r.step_dma0)).unwrap();
 
     let mut ticker = Ticker::every(Duration::from_secs(1));
     loop {
@@ -181,7 +188,7 @@ impl ReceiverHandler for Handler {
 }
 
 #[embassy_executor::task]
-async fn usb_comm_task(driver: embassy_rp::usb::Driver<'static, USB>) {
+async fn usb_comm_task(driver: embassy_rp::usb::Driver<'static, pUSB>) {
     embassy_usb_logger::run!({ 2 << 12 }, log::LevelFilter::Trace, driver, Handler);
 }
 
@@ -191,12 +198,15 @@ enum ControllerCommand {
 }
 
 #[embassy_executor::task]
-async fn controller(mut x_sm: StateMachine<'static, PIO0, 0>, x_step_buf: peripherals::DMA_CH0) {
-    let machine = SystemState {
-        current_pos: [0.0; 3],
-        current_speed: 0.0,
-    };
+async fn step_consumer(mut sm0: StateMachine<'static, pPIO0, 0>, dma0: peripherals::DMA_CH0) {
+    let mut dma0_ref = dma0.into_ref();
+    loop {
+        let StepBuffer(buffer) = STEP_QUEUE.receive().await;
+        sm0.tx().dma_push(dma0_ref.reborrow(), &buffer).await;
+    }
+}
 
+async fn execute_motion(machine: &mut SystemState, command: MotionCommand) {
     let accel = 100_000.0f32;
 
     let micro_steps = 16;
@@ -208,138 +218,129 @@ async fn controller(mut x_sm: StateMachine<'static, PIO0, 0>, x_step_buf: periph
         steps_per_mm[2] * micro_steps as f32,
     ];
 
-    let mut x_step_buf_ref = x_step_buf.into_ref();
-    let mut ring = RingBuffer::new();
+    let [mut x0, mut y0, mut z0] = machine.current_pos;
+
+    let (x1, y1, z1, feed_rate) = match command {
+        MotionCommand::Linear([x, y, z], feed_rate) => (x, y, z, feed_rate),
+    };
+
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+    let dz = z1 - z0;
+
+    let distance = dist3d(x0, y0, z0, x1, y1, z1);
+
+    let mut traveled = 0.0;
+    let mut current_vel = 0.0; // or carry over from prior block
+    let max_vel = feed_rate; // in, say, mm/s
+    let dt = 0.0001; // 1ms
+    let mut is_decelerating = false;
+
+    let epsilon = 0.001; // mm
+
+    let mut delay_accum: f32 = 0.0;
+
+    let mut buffer = [0u32; 1024];
+    let mut buffer_idx = 0;
+
+    while traveled < distance - epsilon {
+        while STEP_QUEUE.is_full() {
+            Timer::after(Duration::from_millis(1)).await;
+        }
+
+        let dist_left = distance - traveled;
+
+        if is_decelerating {
+            current_vel -= accel * dt;
+            if current_vel < 0.0 {
+                break;
+            }
+        } else {
+            let decel_dist = (current_vel * current_vel) / (2.0 * accel);
+            if decel_dist >= dist_left {
+                is_decelerating = true;
+            } else if current_vel < max_vel {
+                current_vel += accel * dt;
+                if current_vel > max_vel {
+                    current_vel = max_vel;
+                }
+            }
+        }
+
+        let delta_dist = current_vel * dt;
+        traveled += delta_dist;
+
+        let frac = delta_dist / distance;
+
+        let step_x_mm = dx * frac;
+        let step_y_mm = dy * frac;
+        let step_z_mm = dz * frac;
+
+        let steps_x = step_x_mm * steps_per_mm[0];
+        let steps_y = step_y_mm * steps_per_mm[1];
+        let steps_z = step_z_mm * steps_per_mm[2];
+
+        x0 += step_x_mm;
+        y0 += step_y_mm;
+        z0 += step_z_mm;
+
+        // let dir_x = if steps_x >= 0 { 1 } else { 0 };
+        // let dir_y = if steps_y >= 0 { 1 } else { 0 };
+        // let dir_z = if steps_z >= 0 { 1 } else { 0 };
+
+        let steps_x_abs = fabsf(steps_x);
+        // let steps_y_abs = steps_y.abs() as u32;
+        // let steps_z_abs = steps_z.abs() as u32;
+
+        let rate_x = (steps_x_abs as f32) / dt; // steps/s
+                                                // let rate_y = (steps_y_abs as f32) / dt; // steps/s
+                                                // let rate_z = (steps_z_abs as f32) / dt; // steps/s
+
+        let step_period_x = ((1200 * 200_000) as f32 / rate_x);
+        // let step_period_y = ((1200 * 200_000) as f32 / rate_y) as u32;
+        // let step_period_z = ((1200 * 200_000) as f32 / rate_z) as u32;
+
+        delay_accum += step_period_x;
+
+        // Extract the integer part to use as the delay
+        let delay_cycles = floorf(delay_accum) as u32;
+
+        // Subtract the integer part, leaving the fractional remainder
+        delay_accum -= delay_cycles as f32;
+
+        log::info!(
+            "pos: {}, speed: {}, rate: {}, period: {}",
+            x0,
+            current_vel,
+            rate_x,
+            delay_cycles
+        );
+
+        if steps_x_abs > 0.0 {
+            buffer[buffer_idx] = steps_x_abs as u32;
+            buffer[buffer_idx + 1] = delay_cycles;
+            buffer_idx += 2;
+        }
+
+        if buffer_idx >= buffer.len() {
+            STEP_QUEUE.send(StepBuffer(buffer)).await;
+            buffer_idx = 0;
+        }
+
+        Timer::after(Duration::from_micros(100)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn controller() {
+    let mut machine = SystemState {
+        current_pos: [0.0; 3],
+        current_speed: 0.0,
+    };
 
     loop {
         if let Ok(block) = MOTION_QUEUE.try_receive() {
-            let [mut x0, mut y0, mut z0] = machine.current_pos;
-
-            let (x1, y1, z1, feed_rate) = match block {
-                MotionCommand::Linear([x, y, z], feed_rate) => (x, y, z, feed_rate),
-            };
-
-            let dx = x1 - x0;
-            let dy = y1 - y0;
-            let dz = z1 - z0;
-
-            let distance = dist3d(x0, y0, z0, x1, y1, z1);
-
-            let mut traveled = 0.0;
-            let mut current_vel = 0.0; // or carry over from prior block
-            let max_vel = feed_rate; // in, say, mm/s
-            let dt = 0.0001; // 1ms
-            let mut is_decelerating = false;
-
-            let epsilon = 0.001; // mm
-
-            let mut delay_accum: f32 = 0.0;
-
-            while traveled < distance - epsilon {
-                if ring.len() > 0 {
-                    let contiguous = ring.contiguous_data();
-                    x_sm.tx()
-                        .dma_push(x_step_buf_ref.reborrow(), contiguous)
-                        .await;
-                    let len = contiguous.len();
-                    ring.consume(len);
-                    log::info!("Pushed {} steps", len);
-                }
-
-                let dist_left = distance - traveled;
-
-                if is_decelerating {
-                    current_vel -= accel * dt;
-                    if current_vel < 0.0 {
-                        break;
-                    }
-                } else {
-                    let decel_dist = (current_vel * current_vel) / (2.0 * accel);
-                    if decel_dist >= dist_left {
-                        is_decelerating = true;
-                    } else if current_vel < max_vel {
-                        current_vel += accel * dt;
-                        if current_vel > max_vel {
-                            current_vel = max_vel;
-                        }
-                    }
-                }
-
-                let delta_dist = current_vel * dt;
-                traveled += delta_dist;
-
-                let frac = delta_dist / distance;
-
-                let step_x_mm = dx * frac;
-                let step_y_mm = dy * frac;
-                let step_z_mm = dz * frac;
-
-                let steps_x = step_x_mm * steps_per_mm[0];
-                let steps_y = step_y_mm * steps_per_mm[1];
-                let steps_z = step_z_mm * steps_per_mm[2];
-
-                x0 += step_x_mm;
-                y0 += step_y_mm;
-                z0 += step_z_mm;
-
-                // let dir_x = if steps_x >= 0 { 1 } else { 0 };
-                // let dir_y = if steps_y >= 0 { 1 } else { 0 };
-                // let dir_z = if steps_z >= 0 { 1 } else { 0 };
-
-                let steps_x_abs = fabsf(steps_x);
-                // let steps_y_abs = steps_y.abs() as u32;
-                // let steps_z_abs = steps_z.abs() as u32;
-
-                let rate_x = (steps_x_abs as f32) / dt; // steps/s
-                                                        // let rate_y = (steps_y_abs as f32) / dt; // steps/s
-                                                        // let rate_z = (steps_z_abs as f32) / dt; // steps/s
-
-                let step_period_x = ((1200 * 200_000) as f32 / rate_x);
-                // let step_period_y = ((1200 * 200_000) as f32 / rate_y) as u32;
-                // let step_period_z = ((1200 * 200_000) as f32 / rate_z) as u32;
-
-                delay_accum += step_period_x;
-
-                // Extract the integer part to use as the delay
-                let delay_cycles = floorf(delay_accum) as u32;
-
-                // Subtract the integer part, leaving the fractional remainder
-                delay_accum -= delay_cycles as f32;
-
-                log::info!(
-                    "pos: {}, speed: {}, rate: {}, period: {}",
-                    x0,
-                    current_vel,
-                    rate_x,
-                    delay_cycles
-                );
-
-                if steps_x_abs > 0.0 {
-                    // It is safe to unwrap here because we waited for free space.
-                    ring.push(steps_x_abs as u32).unwrap();
-                    ring.push(delay_cycles).unwrap();
-                }
-
-                // y_sm.tx().wait_push(rate_y as u32).await;
-                // y_sm.tx().wait_push(step_period_y).await;
-
-                // z_sm.tx().wait_push(rate_z as u32).await;
-                // z_sm.tx().wait_push(step_period_z).await;
-
-                Timer::after(Duration::from_micros(100)).await;
-            }
-
-            while ring.len() > 0 {
-                let contiguous = ring.contiguous_data();
-                x_sm.tx()
-                    .dma_push(x_step_buf_ref.reborrow(), contiguous)
-                    .await;
-                ring.consume(contiguous.len());
-            }
-
-            log::info!("Done moving");
-        } else {
-            log::info!("No motion command");
+            execute_motion(&mut machine, block).await;
         }
 
         match CONTROLLER_CHANNEL.receive().await {
@@ -353,61 +354,6 @@ async fn controller(mut x_sm: StateMachine<'static, PIO0, 0>, x_step_buf: periph
                 log::info!("!!Stopping!!");
             }
         }
-    }
-}
-
-struct RingBuffer {
-    buffer: [u32; 256],
-    head: usize,
-    tail: usize,
-}
-
-impl RingBuffer {
-    fn new() -> Self {
-        Self {
-            buffer: [0; 256],
-            head: 0,
-            tail: 0,
-        }
-    }
-
-    // Number of items currently stored.
-    fn len(&self) -> usize {
-        if self.head >= self.tail {
-            self.head - self.tail
-        } else {
-            self.buffer.len() - self.tail + self.head
-        }
-    }
-
-    // Free space remaining.
-    fn free_space(&self) -> usize {
-        self.buffer.len() - self.len()
-    }
-
-    // Push a value into the ring buffer. Returns Err if no space.
-    fn push(&mut self, item: u32) -> Result<(), ()> {
-        if self.free_space() == 0 {
-            Err(())
-        } else {
-            self.buffer[self.head] = item;
-            self.head = (self.head + 1) % self.buffer.len();
-            Ok(())
-        }
-    }
-
-    // Returns a contiguous slice from tail up to the end (or head if no wrap-around).
-    fn contiguous_data(&self) -> &[u32] {
-        if self.head >= self.tail {
-            &self.buffer[self.tail..self.head]
-        } else {
-            &self.buffer[self.tail..self.buffer.len()]
-        }
-    }
-
-    // Mark `count` items as consumed.
-    fn consume(&mut self, count: usize) {
-        self.tail = (self.tail + count) % self.buffer.len();
     }
 }
 
